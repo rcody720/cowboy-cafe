090a277fd846a1e03649b6862768f35ddcec204f
Cody Reeves <rcody720@ksu.edu>
2020-02-26 15:03:51 -0600

    Completed Order class fields

--- Data/Order.cs
+++ Data/Order.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CowboyCafe.Data
 {
     public class Order
     {
+        private uint lastOrderNumber;
+
+        private List<IOrderItem> items;
+
         public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
 
         public double Subtotal => 0;
+
+        public uint OrderNumber { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public void Add(IOrderItem item) { }
 
