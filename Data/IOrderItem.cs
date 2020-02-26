480a5b10adba593bf2d2e47f248987ab9bed9b06
Cody Reeves <rcody720@ksu.edu>
2020-02-26 14:52:12 -0600

    Finished OrderTests

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// An interface representing a single item in an order
+        /// </summary>
+        double Price { get; }
+
+        //The special instructions for this order item
+        List<string> SpecialInstructions { get; }
+    }
+}
