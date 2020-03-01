609d8d869f81cc02354835025eca479c9adb421e
Cody Reeves <rcody720@gmail.com>
2020-03-01 16:07:20 -0600

    SpecialInstructions Show up

--- Data/Side.cs
+++ Data/Side.cs
@@ -35,6 +35,6 @@ namespace CowboyCafe.Data
         /// </summary>
         public abstract uint Calories { get; }
 
-        public List<string> SpecialInstructions => throw new NotImplementedException();
+        public List<string> SpecialInstructions { get; }
     }
 }
