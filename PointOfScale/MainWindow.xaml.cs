6a3f211273060a7edb97c6248a90bc9f401cb68d
rcody720 <49534252+rcody720@users.noreply.github.com>
2020-03-01 20:42:05 -0600

    Added comments

--- PointOfScale/MainWindow.xaml.cs
+++ PointOfScale/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+/*
+* Author: Cody Reeves
+*
+* Class name: MainWindow.xaml.cs
+*
+* Purpose: A class that controls the backend of the user interface
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +29,9 @@ namespace PointOfSale
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes components and the Data Context
+        /// <summary>
         public MainWindow()
         {
             InitializeComponent();
