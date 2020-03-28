1a22bfe05a59b78a4426f8230b76a8002f978f84
Cody Reeves <rcody720@gmail.com>
2020-03-28 00:46:32 -0500

    POS 4 Pt 1 almost complete

--- PointOfScale/OrderSummaryControl.xaml.cs
+++ PointOfScale/OrderSummaryControl.xaml.cs
@@ -42,9 +42,65 @@ namespace PointOfSale
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         { 
             var orderControl = this.FindAncestor<OrderControl>();
-            if (DataContext is AngryChicken data)
+            
+            foreach(object item in e.AddedItems)
             {
-                orderControl.SwapScreen(CustomizeAngryChicken);
+                if (item is AngryChicken)
+                {                   
+                    orderControl.SwapScreen(new CustomizeAngryChicken());
+                }
+                else if (item is CowpokeChili)
+                {
+                    orderControl.SwapScreen(new CustomizeCowpokeChili());
+                }
+                else if (item is DakotaDoubleBurger)
+                {
+                    orderControl.SwapScreen(new CustomizeDakotaDoubleBurger());
+                }
+                else if (item is PecosPulledPork)
+                {
+                    orderControl.SwapScreen(new CustomizePecosPulledPork());
+                }
+                else if (item is TexasTripleBurger)
+                {
+                    orderControl.SwapScreen(new CustomizeTexasTripleBurger());
+                }
+                else if (item is TrailBurger)
+                {
+                    orderControl.SwapScreen(new CustomizeTrailBurger());
+                }
+                else if (item is BakedBeans)
+                {
+                    orderControl.SwapScreen(new CustomizeBakedBeans());
+                }
+                else if (item is ChiliCheeseFries)
+                {
+                    orderControl.SwapScreen(new CustomizeChiliCheeseFries());
+                }
+                else if (item is CornDodgers)
+                {
+                    orderControl.SwapScreen(new CustomizeCornDodgers());
+                }
+                else if (item is PanDeCampo)
+                {
+                    orderControl.SwapScreen(new CustomizePanDeCampo());
+                }
+                else if (item is CowboyCoffee)
+                {
+                    orderControl.SwapScreen(new CustomizeCowboyCoffee());
+                }
+                else if (item is JerkedSoda)
+                {
+                    orderControl.SwapScreen(new CustomizeJerkedSoda());                        
+                }
+                else if (item is TexasTea)
+                {
+                    orderControl.SwapScreen(new CustomizeTexasTea());
+                }
+                else if (item is Water)
+                {
+                    orderControl.SwapScreen(new CustomizeWater());
+                }
             }
         }
     }
