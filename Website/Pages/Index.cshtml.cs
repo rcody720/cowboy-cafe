b0153af99b0a80d43be07eefb049e6e53efb1b87
Cody Reeves <rcody720@ksu.edu>
2020-05-04 18:54:05 -0500

    Almost LINQ

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -16,36 +16,42 @@ namespace Website.Pages
         /// <summary>
         /// The items to display on the index page
         /// </summary>
-        public IEnumerable<IOrderItem> Items { get; protected set; } = Menu.CompleteMenu();
+        public IEnumerable<IOrderItem> Items { get; protected set; }
 
         /// <summary>
         /// The current search terms
-        /// </summary>        
+        /// </summary>  
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; }
 
         /// <summary>
         /// The filtered categories
         /// </summary>      
+        [BindProperty(SupportsGet = true)]
         public string[] Categories { get; set; }
 
         /// <summary>
         /// The minimum calories
         /// </summary> 
+        [BindProperty(SupportsGet = true)]
         public int? CaloriesMin { get; set; }
 
         /// <summary>
         /// The maximum calories
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? CaloriesMax { get; set; }
 
         /// <summary>
         /// The minimum price
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public double? PriceMin { get; set; }
 
         /// <summary>
         /// The maximum price
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public double? PriceMax { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -53,18 +59,45 @@ namespace Website.Pages
             _logger = logger;
         }
 
-        public void OnGet(int? caloriesMin, int? caloriesMax, double? priceMin, double? priceMax)
+        public void OnGet()
         {
-            CaloriesMin = caloriesMin;
-            CaloriesMax = caloriesMax;
-            PriceMin = priceMin;
-            PriceMax = priceMax;
-            SearchTerms = Request.Query["SearchTerms"];
-            Categories = Request.Query["Categories"];
-            Items = Menu.Search(Items, SearchTerms);
-            Items = Menu.FilterByCategory(Items, Categories);
-            Items = Menu.FilterByCalories(Items, CaloriesMin, CaloriesMax);
-            Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
+            //CaloriesMin = caloriesMin;
+            //CaloriesMax = caloriesMax;
+            //PriceMin = priceMin;
+            //PriceMax = priceMax;
+            //SearchTerms = Request.Query["SearchTerms"];
+            //Categories = Request.Query["Categories"];
+            //Items = Menu.Search(Items, SearchTerms);
+            //Items = Menu.FilterByCategory(Items, Categories);
+            //Items = Menu.FilterByCalories(Items, CaloriesMin, CaloriesMax);
+            //Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
+
+            Items = Menu.CompleteMenu();
+            if (SearchTerms != null)
+            {
+                Items = Items.Where(item => item.ToString() != null && item.ToString().Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            if(Categories != null && Categories.Length != 0)
+            {
+                Items = Items.Where(item => Categories.Contains(item.GetType().BaseType.Name));
+            }
+
+            if (CaloriesMin == null && CaloriesMax != null)
+            {
+                Items = Items.Where(item => item.Calories <= CaloriesMax);
+            }
+
+            if (CaloriesMax == null && CaloriesMin != null)
+            {
+                Items = Items.Where(item => item.Calories >= CaloriesMin);
+            }
+
+            if (CaloriesMax != null && CaloriesMin != null)
+            {
+
+                Items = Items.Where(item => item.Calories >= CaloriesMin && item.Calories <= CaloriesMax);
+            }
         }      
     }
 }
